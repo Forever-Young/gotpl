2d8c56242b6d08255ead79c6e1415717d94844a2
Anton Novosyolov <anton.novosyolov@gmail.com>
2016-09-28 11:56:40 +0300

    Read from env variables. Default to blank string

--- tpl.go
+++ tpl.go
@@ -1,35 +1,28 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 	"text/template"
-
-	"gopkg.in/yaml.v2"
 )
 
-// Reads a YAML document from the values_in stream, uses it as values
-// for the tpl_files templates and writes the executed templates to
-// the out stream.
-func ExecuteTemplates(values_in io.Reader, out io.Writer, tpl_files ...string) error {
+// Reads environment variables, uses them as values for the tpl_files templates
+// and writes the executed templates to the out stream.
+func ExecuteTemplates(out io.Writer, tpl_files ...string) error {
 	tpl, err := template.ParseFiles(tpl_files...)
 	if err != nil {
 		return fmt.Errorf("Error parsing template(s): %v", err)
 	}
 
-	buf := bytes.NewBuffer(nil)
-	_, err = io.Copy(buf, values_in)
-	if err != nil {
-		return fmt.Errorf("Failed to read standard input: %v", err)
-	}
+	tpl.Option("missingkey=zero")
 
-	var values map[string]interface{}
-	err = yaml.Unmarshal(buf.Bytes(), &values)
-	if err != nil {
-		return fmt.Errorf("Failed to parse standard input: %v", err)
+	values := make(map[string]string)
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		values[pair[0]] = pair[1]
 	}
 
 	err = tpl.Execute(out, values)
@@ -40,7 +33,7 @@ func ExecuteTemplates(values_in io.Reader, out io.Writer, tpl_files ...string) e
 }
 
 func main() {
-	err := ExecuteTemplates(os.Stdin, os.Stdout, os.Args[1:]...)
+	err := ExecuteTemplates(os.Stdout, os.Args[1:]...)
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
